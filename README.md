7386e5727a4c03e80d64034eda7ec019de1bd7c0
Binod Chakradhar <binod.chakradhar@ebpearls.com>
2026-03-30 17:35:16 +0545

    supabase data fetch

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
+
+table smoothies with rows
+    id (auto-increment)
+    created_at (date)
+    title (text)
+    method (text)
+    rating (int8)
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,37 @@
+import { useEffect, useState } from 'react'
 import supabase from '../config/supabaseClient'
 
 const Home = () => {
+  // console.log(supabase)
+  const [fetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from('smoothies').select()
+      if (error) {
+        setFetchError('Could not fetch smoothies')
+        setSmoothies(null)
+        console.log(error)
+      }
+      if (data) {
+        setSmoothies(data)
+        setFetchError(null)
+      }
+    }
+    fetchSmoothies()
+  }, [])
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && (<p>{fetchError}</p>)}
+      {smoothies && (
+        <ul>
+          {smoothies.map(smoothie => (
+            <p key={smoothie.id}>{smoothie.title}</p>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
